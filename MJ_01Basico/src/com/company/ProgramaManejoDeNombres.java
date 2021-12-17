140ebb1488d32877556159200c3678a47082903b
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-12-17 21:53:36 +0100

    fixes and task 3

---
+++ MJ_01Basico/src/com/company/ProgramaManejoDeNombres.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class ProgramaManejoDeNombres {
+
+    public static void main(String[] args) {
+
+        String[] Familiares = args;
+        final int numNombres = 3;
+
+        if(args.length == numNombres) {
+            for (int i = 0; i < args.length; i++) {
+                String segundoCaracter = Familiares[i].substring(1, 2).toUpperCase();
+                String nombreGenerado = segundoCaracter.concat(".").concat(Familiares[i].substring(Familiares[i].length()-2));
+                StringBuilder nombresGeneradosFamiliares = new StringBuilder(nombreGenerado);
+                System.out.println(nombresGeneradosFamiliares);
+            }
+        } else {
+            System.out.println("Se debe introducir 3 argumentos.");
+        }
+    }
+}
+
+
