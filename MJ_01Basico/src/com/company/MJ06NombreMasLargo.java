2c56df12d64267fbbb58f4083d643753d48c8342
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-12-31 20:21:23 +0100

    Update MJ06NombreMasLargo.java

--- MJ_01Basico/src/com/company/MJ06NombreMasLargo.java
+++ MJ_01Basico/src/com/company/MJ06NombreMasLargo.java
@@ -23,5 +23,10 @@ public class MJ06NombreMasLargo {
         } else {
             JOptionPane.showMessageDialog(null, "El nombre más largo es: " + nombre3);
         }
+
+        //Otra forma
+        //String max = (nombre1.split(" ")[0].length() < nombre2.split(" ")[0].length()) ? nombre2 : nombre1;
+        //max = (nombre3.split(" ")[0].length() < max.split(" ")[0].length()) ? max: nombre3;
+        //System.out.println("La persona con el nombre mas largo es " + max);
     }
 }
