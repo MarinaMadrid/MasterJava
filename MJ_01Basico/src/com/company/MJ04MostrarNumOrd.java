03b8a75c61471418655c9122e11a9835c2233c63
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-12-31 17:54:29 +0100

    fixes and task 4

---
+++ MJ_01Basico/src/com/company/MJ04MostrarNumOrd.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MJ04MostrarNumOrd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un número:");
+        Float num1  = scanner.nextFloat();
+
+        System.out.println("Ingrese otro número:");
+        Float num2  = scanner.nextFloat();
+
+        Float max = (num1 > num2)? num1 : num2;
+        Float min = (num1 > num2)? num2 : num1;
+
+        String resultado = "Los números ordenados de mayor a menor son: \n" + max + "\n" + min;
+        System.out.println(resultado);
+
+        //String resultado = (num1 > num2) ? num1 + " y " + num2 : num2 + " y " + num1;
+        //System.out.println("El orden es: " + resultado);
+    }
+}
