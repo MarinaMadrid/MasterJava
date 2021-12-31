79eb6bacd271c20d00abe10772e8b0ec64b9084f
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-12-31 17:55:38 +0100

    change name

--- MJ_01Basico/src/com/company/MJ04MostrarNumerosOrdenados.java
+++ MJ_01Basico/src/com/company/MJ04MostrarNumerosOrdenados.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class MJ04MostrarNumOrd {
+public class MJ04MostrarNumerosOrdenados {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
